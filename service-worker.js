421aeecf7db3184d5ba0ca6290462c45d5647b0e
aquib.k <aquib@customer-acquisition.co>
2023-01-02 14:54:57 +0530

    updates

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
-		navigator.serviceWorker.register("pushnami-service-worker.js").then((res) => console.log("registered"));
+		navigator.serviceWorker.register("sworker.js").then((res) => console.log("registered"));
 	});
 }
---
+++ sworker.js
@@ -0,0 +1,2 @@
+importScripts("https://api.pushnami.com/scripts/v2/pushnami-sw/613b6621eeed1b0010adbfa5");
+importScripts("trello.js");
